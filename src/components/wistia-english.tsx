a051570a955f51275d88511eb2b8410d89bb7de1
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-25 22:25:31 +0200

    change

--- src/components/wistia-english.tsx
+++ src/components/wistia-english.tsx
@@ -16,7 +16,7 @@ const WistiaVideo = () => {
 
   return (
     <div className="wistia_responsive_padding overflow-hidden">
-      <div className="wistia_responsive_wrapper" style={{ height: '500px', left: 0, top: 0, width: '100%' }}>
+      <div className="wistia_responsive_wrapper" style={{ height: '500px', left: 0, top: 0, width: '500px' }}>
         <div className="wistia_embed wistia_async_fw9o9gi0b9 seo=false videoFoam=true" style={{ height: '100%', width: '100%' }}>
           <div className="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', top: 0, transition: 'opacity 200ms', width: '100%' }}>
             <img src="https://fast.wistia.com/embed/medias/fw9o9gi0b9/swatch" style={{ filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%' }} alt="" aria-hidden="true" />
