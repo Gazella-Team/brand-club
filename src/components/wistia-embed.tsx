a09b93dd53758fbd527a313da345b6229b2de541
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-29 21:11:46 +0200

    PUSH

--- src/components/wistia-embed.tsx
+++ src/components/wistia-embed.tsx
@@ -1,30 +1,33 @@
 import React, { useEffect } from 'react';
 
-const WistiaVideo = () => {
+const WistiaEmbed = () => {
   useEffect(() => {
-    // Load Wistia script dynamically
-    const wistiaScript = document.createElement('script');
-    wistiaScript.src="https://fast.wistia.com/assets/external/E-v1.js";
-    wistiaScript.async = true;
-    document.body.appendChild(wistiaScript);
+    const script1 = document.createElement('script');
+    script1.src = 'https://fast.wistia.com/embed/medias/ujsoark3fi.jsonp';
+    script1.async = true;
+
+    const script2 = document.createElement('script');
+    script2.src = 'https://fast.wistia.com/assets/external/E-v1.js';
+    script2.async = true;
+
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
 
     return () => {
-      // Cleanup function to remove the script when the component is unmounted
-      document.body.removeChild(wistiaScript);
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
     };
   }, []);
 
   return (
-    <div className="wistia_responsive_padding overflow-hidden">
-      <div className="wistia_responsive_wrapper" style={{ height: '500px', left: 0, top: 0, width: '100%' }}>
-        <div className="wistia_embed wistia_async_fw9o9gi0b9 seo=false videoFoam=true" style={{ height: '100%', width: '100%' }}>
-          <div className="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', top: 0, transition: 'opacity 200ms', width: '100%' }}>
-            <img src="https://fast.wistia.com/embed/medias/ujsoark3fi/swatch" style={{ filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%' }} alt="" aria-hidden="true" />
-          </div>
+    <div className="wistia_responsive_padding">
+      <div className="wistia_responsive_wrapper w-[400px] sm:h-[600px] sm:w-[600px]" style={{left: 0, top: 0}}>
+        <div className="wistia_embed wistia_async_ujsoark3fi seo=false videoFoam=true" style={{ height: '100%', position: 'relative', width: '100%' }}>
+          &nbsp;
         </div>
       </div>
     </div>
   );
 };
 
-export default WistiaVideo;
+export default WistiaEmbed;
--- src/components/wistia-english.tsx
+++ src/components/wistia-english.tsx
@@ -1,24 +1,29 @@
 import React, { useEffect } from 'react';
 
-const EnglishWistiaVideo = () => {
+const EnglishWistiaEmbed = () => {
   useEffect(() => {
-    // Load Wistia script dynamically
-    const wistiaScript = document.createElement('script');
-    wistiaScript.src = 'https://fast.wistia.com/assets/external/E-v1.js';
-    wistiaScript.async = true;
-    document.body.appendChild(wistiaScript);
+    const script1 = document.createElement('script');
+    script1.src = 'https://fast.wistia.com/embed/medias/fw9o9gi0b9.jsonp';
+    script1.async = true;
+
+    const script2 = document.createElement('script');
+    script2.src = 'https://fast.wistia.com/assets/external/E-v1.js';
+    script2.async = true;
+
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
 
     return () => {
-      // Cleanup function to remove the script when the component is unmounted
-      document.body.removeChild(wistiaScript);
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
     };
   }, []);
 
   return (
-    <div className="wistia_responsive_padding overflow-hidden">
-      <div className="wistia_responsive_wrapper sm:h-[200px]" style={{ height: '500px', left: 0, top: 0, width: '100%' }}>
-        <div className="wistia_embed wistia_async_fw9o9gi0b9 seo=false videoFoam=true" style={{ height: '100%', width: '100%' }}>
-          <div className="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', top: 0, transition: 'opacity 200ms', width: '100%' }}>
+    <div className="wistia_responsive_padding">
+      <div className="wistia_responsive_wrapper w-[400px] sm:h-[600px] sm:w-[600px]" style={{ left: 0, top: 0, }}>
+        <div className="wistia_embed wistia_async_fw9o9gi0b9 seo=false videoFoam=true" style={{ height: '100%', position: 'relative', width: '100%' }}>
+          <div className="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%' }}>
             <img src="https://fast.wistia.com/embed/medias/fw9o9gi0b9/swatch" style={{ filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%' }} alt="" aria-hidden="true" />
           </div>
         </div>
@@ -27,4 +32,4 @@ const EnglishWistiaVideo = () => {
   );
 };
 
-export default EnglishWistiaVideo;
+export default EnglishWistiaEmbed;
