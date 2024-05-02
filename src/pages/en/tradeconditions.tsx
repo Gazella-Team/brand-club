b759a2842e5035ca4a35ee6a3129930646943c4f
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-02 18:51:04 +0200

    changes

--- src/pages/en/tradeconditions.tsx
+++ src/pages/en/tradeconditions.tsx
@@ -22,74 +22,56 @@ export default function TradeConditions() {
       <section className="py-[150px] pt-[250px]">
         <div className="w-[90%] mx-auto max-w-[800px]">
           <h1 className="text-[50px] font-[700] mb-[30px]">
-          Terms and conditions
+          Trading conditions          
           </h1>
           <p className="mb-[100px] text-[18px]">
-          When you work with OCMA Masterclass, you know that everything is carefully structured and executed. This includes both the product we have created and are very proud of, as well as the legal aspects. For this reason, we have clarified all the points you accept when entering into an agreement with OCMA Masterclass. Primarily, this is self-evident and is perceived in our view as normal business practice. Do not hesitate to contact us at benjamin@ocmamasterclass.com if you have any questions.
-            <br></br>
-            <br></br>
-            We typically collect and process the following types of information:
-            A unique ID and technical information about your computer, tablet or
-            mobile phone, your IP number, geographical location and which pages
-            you click on (interests). To the extent that you explicitly consent
-            to this and enter the information yourself, we also process Name,
-            phone number, e-mail, address and payment information. This will
-            typically be in connection with creating a login or making a
-            purchase.
-          </p>
+          When you work with OCMA Masterclass, you know that everything is carefully structured and executed. This includes both the product we have created and are very proud of, as well as the legal aspects. For this reason, we have clarified all the points you accept when entering into an agreement with OCMA Masterclass. Primarily, this is self-evident and is perceived in our view as normal business practice. Do not hesitate to contact us at benjamin@ocmamasterclass.com if you have any questions.          </p>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Offer and agreement
             </h1>
             <p className="text-[18px]">
-            The invoice/payment link for the agreement/purchase/order will be sent as soon as agreement is reached. It is considered accepted, and thus no right of withdrawal applies, as soon as the customer receives the invoice, unless the customer notifies in writing that something is wrong. In particular, payment of the invoice is considered accepted, and no right of withdrawal applies. (Remember, however, that we have a satisfaction guarantee, read further down)
-            </p>
+            The invoice/payment link for the agreement/purchase/order/order will be sent as soon as agreement is reached. It is considered accepted, and thus no right of withdrawal applies, as soon as the customer receives the invoice, unless the customer notifies in writing that something is wrong. In particular, payment of the invoice is considered accepted, and no right of withdrawal applies.             </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Prices and payment
             </h1>
             <p className="text-[18px]">
-            If the payment deadline stated on the invoice is exceeded,OCMA masterclass reserves the right to impose reminder fees and collection costs on the customer.
-            </p>
+            If the payment deadline stated on the invoice is exceeded, OCMA Masterclass reserves the right to impose reminder fees and collection costs on the customer            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">Responsibility</h1>
             <p className="text-[18px]">
-            The customer is responsible for the legality of the products' content and design in both public and private law contexts. It must be emphasized that OCMA Masterclass does not actively assess the legality of marketing initiatives of any kind, regardless of the extent to which OCMA Masterclass has contributed to their design.
-            </p>
+            The customer/student is responsible for accepting that this masterclass is a product for adults over the age of 18, at the same time as the legality of the products' content and design in both public and private law contexts. It must be emphasized that OCMA Masterclass does not actively assess the legality of marketing initiatives of any kind, regardless of the extent to which Ocma Masterclass has contributed to their design. OCMA Masterclass cannot be held responsible for students' breaches of the Marketing Act.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Limited liability
             </h1>
             <p className="text-[18px]">
-            In the event of delay or defects in the delivered, OCMA Masterclass does not assume responsibility for operating loss, loss of profit or other indirect loss, including loss as a result of the customer's legal relationship with third parties. OCMA Masterclass can under no circumstances assume responsibility for an amount greater than what has been paid for the purchase of our masterclass
-            </p>
+            In the event of delay or deficiencies in the delivered, OCMA Masterclass does not assume responsibility for operating loss, loss of profit or other indirect loss, including loss as a result of the customer's legal relationship with third parties. OCMA Masterclass can under no circumstances assume responsibility for an amount greater than what has been paid for the purchase of our masterclass            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               When purchasing the OCMA Masterclass
             </h1>
             <p className="text-[18px]">
-            When you buy a Masterclass from OCMA Masterclass, you also undertake not to distribute material or the like in any way to people who have not bought the course. If you do this, then OCMA Masterclass is entitled to make a claim for damages and kick you out of the group and shut down your access. See more below.
-            </p>
+            When you buy a Masterclass from OCMA Masterclass, you also undertake not to distribute material or similar in any way to people who have not bought the course. If you do this, then OCMA Masterclass is entitled to make a compensation claim and kick you out of the group and shut down your access. See more below.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Right of use and ownership
             </h1>
             <p className="text-[18px]">
-            The customer obtains full right of use for the specific content and the associated templates. The customer is defined here as the person who acquires the service, either as a representative of a company or as a private individual. If the content is acquired by a company, the right to use the service will only follow the specific acquirer, who is identified as the contact person for the company at the time of acquisition.
-When the company purchases the product, it is not permitted internally within the company to redistribute the content to employees or co-owners, as this will be considered a breach of the trading conditions. If the need arises to use the product by several people within the same company/legal entity, contact must be made to request offers for the issuance of multiple licenses.
-If a company or individual has already violated the rules for redistribution, before obtaining offers for the issuance of multiple licenses, the issuer reserves the right to invoice the company for a new license, as this is considered a breach of the terms of trade.
-The customer is hereby authorized to do the following:
-Electronically store the original content/template for your own later use (not covered by videos).
-Print the original document/template for your own use.
-Adapt the templates to the given situation and print this/these version(s).
-The customer may not transfer the content/template in question to others, redistribute it on the internet or in other electronic form or in printed (paper) editions without prior permission.
-The customer is under no circumstances entitled to redistribute documents and/or templates.
+            The customer obtains full right of use for the specific content and the associated templates. The customer is defined here as the person who acquires the service, either as a representative of a company or as a private individual. If the content is acquired by a company, the right to use the service will only follow the specific acquirer, who is identified as the contact person for the company at the time of acquisition. When the company purchases the product, it is not permitted internally within the company to redistribute the content to employees or co-owners, as this will be considered a breach of the trading conditions. If the need arises to use the product by several people within the same company/legal entity, contact must be made to request offers for the issuance of multiple licenses. If a company or individual, before obtaining offers for the issuance of multiple licenses, has already violated the rules for redistribution, the issuer reserves the right to invoice the company for a new license, as this is considered a breach of the terms of trade.
 
+The customer is hereby authorized to do the following:
+-Electronically store the original content/template for your own later use (not covered by videos).
+-Print the original document/template for your own use.
+- Adapt the templates to the given situation and print this/these version(s).
+- The customer may not transfer the content/template in question to others, redistribute it on the internet or in other electronic form or in printed (paper) editions without prior permission.
+- The customer is under no circumstances entitled to redistribute documents and/or templates.
             </p>
           </div>
           <div className="mb-[50px]">
@@ -97,16 +79,12 @@ The customer is under no circumstances entitled to redistribute documents and/or
             Guarantee on purchase of OCMA masterclass
             </h1>
             <p className="text-[18px]">
-            You can get all your money refunded after 180 days + 12,000 transferred if you have not achieved an income of NOK 20,000/month. To redeem the guarantee, the following requirements must be met:<br></br>
-              <br></br> - Do min. 20 outreaches per day<br></br>
+            You can get all your money refunded after 180 days if you haven't got a paying customer in your agency. To redeem the guarantee, the following requirements must be met:              <br></br> - Do min. 20 outreaches per day<br></br>
               <br></br> - Attend all weekly calls<br></br>
               <br></br> - See the entire OCMA Masterclass
-              <br></br>
+              <br></br> - make min. 20 outreaches per day
               <br></br> - Be a member of the masterclass for 180 days 
-              <br></br> - Build both your Instagram & Linkedin up to at least 2,000 followers
-              <br></br> This guarantee is offered as we are very proud of our product, we all know enough to be successful with it if they put in the work, we will ensure the success of each student. Your success is our success.
-
-If you have used the guarantee once, you are still welcome to participate in the process at a later time. However, the new price will apply if it has increased in the meantime. When you choose to participate in the course again after using the guarantee, any kind of extra guarantee is waived when purchasing an OCMA masterclass. If you wish to make use of your guarantee, please contact us in writing.
+              <br></br>This guarantee is offered as we are very proud of our product, we all know it will be successful if they put in the work. We will ensure the success of each student. Your success is our success. If you have used the guarantee once, you are still welcome to participate in the process at a later time. However, the new price will apply if it has increased in the meantime. When you choose to participate in the course again after using the guarantee, any kind of extra guarantee is waived when purchasing the OCMA masterclass. If you wish to make use of your guarantee, please contact us in writing.
 
             </p>
           </div>
@@ -115,38 +93,32 @@ If you have used the guarantee once, you are still welcome to participate in the
               Income disclaimer
             </h1>
             <p className="text-[18px]">
-            OCMA masterclass disclaims any responsibility regarding the success of the course participants in earning income through our online course. This is done out of respect for our masterclass. All earnings and income examples on our website and masterclass are purely estimates of what can potentially be achieved. There is no guarantee that you will be able to achieve the examples provided.
-OCMA Masterclass assumes no responsibility for any financial losses that you may incur by using OCMA masterclass product, regardless of our opinion that the price is reasonable in relation to the value of the product.
-
+            OCMA masterclass disclaims any responsibility regarding the success of the course participants in earning income through our online course. This is done out of respect for our masterclass. All earnings and income examples on our website and masterclass are purely estimates of what can potentially be achieved. There is no guarantee that you will be able to achieve the examples provided. OCMA Masterclass assumes no responsibility for any financial loss that you may incur from using the OCMA masterclass product, regardless of our opinion that the price is reasonable in relation to the value of the product.
             </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">Disclaimer</h1>
             <p className="text-[18px]">
-            It is the student's own responsibility to be up-to-date on applicable legislation at all times. OCMA masterclass cannot accept responsibility, either in whole or in part, for any offenses that may arise as a result of the student's actions.
-            </p>
+            It is the student's own responsibility to be up-to-date on applicable legislation at all times. OCMA masterclass cannot accept responsibility, either in whole or in part, for any offenses that may arise as a result of the student's actions.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Privacy and GDPR
             </h1>
             <p className="text-[18px]">
-            Information may not be shared with third parties in any way and may not be used, including after the agreement expires. Both parties undertake to process the information in a way that ensures that it remains inaccessible to unauthorized persons and that there is no risk of unauthorized persons becoming aware of it. It is the customer's responsibility to remove OCMA masterclass´s access to anything related to personal data.
-            </p>
+            Information may not be shared with third parties in any way and may not be used, including after the agreement expires. Both parties undertake to process the information in a way that ensures that it remains inaccessible to unauthorized persons and that there is no risk of unauthorized persons becoming aware of it. It is the customer's responsibility to remove OCMA Masterclass's access to everything relating to personal data.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">Subcontractors</h1>
             <p className="text-[18px]">
-            OCMA masterclass is entitled to fully or partially have work carried out by subcontractor(s).
-            </p>
+            OCMA masterclass is entitled to fully or partially have work carried out by subcontractor(s).            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Choice of law and venue
             </h1>
             <p className="text-[18px]">
-            Any discrepancies that arise as a result of these conditions or other trade relations between the parties must be brought before the court in Copenhagen. The dispute will be settled in accordance with Danish law.
-            </p>
+            Any discrepancies that arise as a result of these conditions or other trade relations between the parties must be brought before the court in Copenhagen. The dispute will be resolved by consensus otherwise under Danish law.            </p>
           </div>
         </div>
       </section>
