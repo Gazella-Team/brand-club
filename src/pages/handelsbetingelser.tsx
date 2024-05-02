feeca7bd1a44320766b49ce3d64b65bbc213d633
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-02 18:24:05 +0200

    trade

--- src/pages/handelsbetingelser.tsx
+++ src/pages/handelsbetingelser.tsx
@@ -31,33 +31,36 @@ export default function Handelsbetingelser() {
               Tilbud og aftale
             </h1>
             <p className="text-[18px]">
-            Fakturaen/betalingslinket for aftalen/købet/bestillingen/ordren bliver sendt, så snart der opnås enighed. Den anses for accepteret, og dermed gælder ingen fortrydelsesret, så snart kunden modtager fakturaen, medmindre kunden skriftligt meddeler, at der er noget galt. Især ved betaling af fakturaen anses det som accepteret, og der gælder ingen fortrydelsesret.            </p>
+            Fakturaen/betalingslinket for aftalen/købet/bestillingen/ordren bliver sendt, så snart der opnås enighed. Den anses for accepteret, og dermed gælder ingen fortrydelsesret, så snart kunden modtager fakturaen, medmindre kunden skriftligt meddeler, at der er noget galt. Især ved betaling af fakturaen anses det som accepteret, og der gælder ingen fortrydelsesret.
+            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Priser og betaling
             </h1>
             <p className="text-[18px]">
-            Hvis betalingsfristen, som er angivet på fakturaen, overskrides, forbeholder OCMA Masterclass sig retten til at pålægge kunden rykkergebyrer og inkassoomkostninger            </p>
+            Hvis betalingsfristen, som er angivet på fakturaen, overskrides, forbeholder OCMA Masterclass sig retten til at pålægge kunden rykkergebyrer og inkassoomkostninger
+            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">Ansvar</h1>
             <p className="text-[18px]">
-Kunden/eleven har ansvaret for at acceptere at denne masterclass er et produkt til voksne over 18 år, samtidig med legaliteten af produkternes indhold og udformning både i offentligretlig og privatretlig sammenhæng. Det skal understreges, at OCMA Masterclass ikke aktivt vurderer lovligheden af markedsføringstiltag af nogen art, uanset i hvilket omfang Ocma Masterclass har medvirket til udformningen af disse. Elevers brud mod markedsføringsloven kan OCMA Masterclass ikke stå til ansvar for.              </p>
+            Kunden/eleven har ansvaret for at acceptere at denne masterclass er et produkt til voksne over 18 år, samtidig med legaliteten af produkternes indhold og udformning både i offentligretlig og privatretlig sammenhæng. Det skal understreges, at OCMA Masterclass ikke aktivt vurderer lovligheden af markedsføringstiltag af nogen art, uanset i hvilket omfang Ocma Masterclass har medvirket til udformningen af disse. Elevers brud mod markedsføringsloven kan OCMA Masterclass ikke stå til ansvar for.
+            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Begrænset hæftelse
             </h1>
             <p className="text-[18px]">
-            Ved forsinkelse eller mangler ved det leverede påtager OCMA Masterclass sig ikke ansvar for driftstab, tab af avance eller andet indirekte tab, herunder tab som følge af kundens retsforhold overfor tredjemand. OCMA Masterclass kan under ingen omstændigheder påtage sig ansvar for et større beløb end det, der er blevet betalt ved købet af vores masterclass</p>          </div>
+            Ved forsinkelse eller mangler ved det leverede påtager OCMA Masterclass sig ikke ansvar for driftstab, tab af avance eller andet indirekte tab, herunder tab som følge af kundens retsforhold overfor tredjemand. OCMA Masterclass kan under ingen omstændigheder påtage sig ansvar for et større beløb end det, der er blevet betalt ved købet af vores masterclass            </p>          
+            </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
             Ved køb af OCMA Masterclass
             </h1>
             <p className="text-[18px]">
-            Når du køber en Masterclass hos OCMA Masterclass, så forpligter du dig samtidig til ikke på nogen vis uddele materiale eller lignende til personer, som ikke har købt forløbet. Hvis du gør dette, så er OCMA Masterclass berettiget til at kræve et erstatningskrav og smide dig ud af gruppen og lukke ned for din adgang. Se mere nedenfor.
-            </p>
+            Når du køber en Masterclass hos OCMA Masterclass, så forpligter du dig samtidig til ikke på nogen vis uddele materiale eller lignende til personer, som ikke har købt forløbet. Hvis du gør dette, så er OCMA Masterclass berettiget til at kræve et erstatningskrav og smide dig ud af gruppen og lukke ned for din adgang. Se mere nedenfor.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
@@ -65,7 +68,6 @@ Kunden/eleven har ansvaret for at acceptere at denne masterclass er et produkt t
             </h1>
             <p className="text-[18px]">
             Kunden opnår fuld brugsret til det specifikke indhold og de tilhørende skabeloner. Kunden defineres her som den person, der erhverver ydelsen, enten som en repræsentant for et firma eller som en privatperson. Hvis indholdet erhverves af en virksomhed, vil brugsretten til ydelsen kun følge den specifikke erhverver, som identificeres som kontaktperson for virksomheden ved erhvervelsen. Ved virksomhedens køb af produktet er det ikke tilladt internt i virksomheden at videredistribuere indholdet til ansatte eller medejere, da dette vil blive anset som et brud på handelsbetingelserne. Hvis behovet opstår for at anvende produktet af flere personer inden for samme virksomhed/juridiske enhed, skal der rettes henvendelse for at anmode om tilbud på udstedelse af flere licenser. Hvis en virksomhed eller privatperson, inden der indhentes tilbud på udstedelse af flere licenser, allerede har overtrådt reglerne for videredistribuering, forbeholder udstederen sig retten til at fakturere virksomheden for en ny licens, da dette betragtes som et brud på handelsbetingelserne.
-
               <br></br>
               <br></br>
               Kunden har herunder tilladelse til at gøre følgende:<br></br>{" "}
@@ -86,39 +88,34 @@ Kunden/eleven har ansvaret for at acceptere at denne masterclass er et produkt t
             Garanti ved køb af OCMA masterclass
             </h1>
             <p className="text-[18px]">
-            Du kan få alle dine penge refunderet efter 180 dage + 12.000 overført såfremt du ikke har opnået en indkomst på 20.000,- / måned For at indløse garantien skal følgende krav opfyldes:<br></br>
-              <br></br> - Lave min. 20 outreaches om dagen<br></br>
+            Du kan få alle dine penge refunderet efter 180 dage såfremt du ikke har fået en betalende kunde i dit bureau. For at indløse garantien skal følgende krav opfyldes:              <br></br> - Lave min. 20 outreaches om dagen<br></br>
               <br></br> - Deltage til alle ugentlige opkald <br></br>
               <br></br> - Set hele OCMA masterclass<br></br>
               <br></br> - Være medlem i min. 180 dage af OCMA masterclass
-              <br></br>Bygge både din instagram & Linkedin op til min 2.000 følgere
               <br></br>
               <br></br> 
-              Denne garanti tilbydes, da vi er meget stolte af vores produkt, vi ved alle nok skal få succes med det, hvis de ligger arbejdet i det. Vi vil sikre hver elevs succes. Din succes er vores succes. Hvis du har gjort brug af garantien én gang, er du stadig velkommen til at deltage i forløbet på et senere tidspunkt. Dog vil den nye pris gælde, hvis den er steget i mellemtiden. Når du vælger at deltage i forløbet igen efter at have benyttet garantien, fraviges enhver form for ekstra garanti ved køb af OCMA masterclass. Ønsker du at gøre brug af din garanti, bedes du kontakte os skriftligt.
-            </p>
+              Denne garanti tilbydes, da vi er meget stolte af vores produkt, vi ved alle nok skal få succes med det, hvis de ligger arbejdet i det. Vi vil sikre hver elevs succes. Din succes er vores succes. Hvis du har gjort brug af garantien én gang, er du stadig velkommen til at deltage i forløbet på et senere tidspunkt. Dog vil den nye pris gælde, hvis den er steget i mellemtiden. Når du vælger at deltage i forløbet igen efter at have benyttet garantien, fraviges enhver form for ekstra garanti ved køb af OCMA masterclass. Ønsker du at gøre brug af din garanti, bedes du kontakte os skriftligt.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Indtægtsfraskrivelse
             </h1>
             <p className="text-[18px]">
-            Alle indtjenings- og indkomst eksempler på vores hjemmeside og masterclass er udelukkende estimater for, hvad der potentielt kan opnås. OCMA Masterclass påtager sig intet ansvar for eventuelle økonomiske tab, som du eventuelt kunne pådrage dig ved brug af OCMA masterclass produkt, uanset vores opfattelse af, at prisen er rimelig i forhold til værdien af produktet.
-            </p>
+            Alle indtjenings- og indkomst eksempler på vores hjemmeside og masterclass er udelukkende estimater for, hvad der potentielt kan opnås. OCMA Masterclass påtager sig intet ansvar for eventuelle økonomiske tab, som du eventuelt kunne pådrage dig ved brug af OCMA masterclass produkt, uanset vores opfattelse af, at prisen er rimelig i forhold til værdien af produktet.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Ansvarsfraskrivelse
             </h1>
             <p className="text-[18px]">
-            Det er elevens eget ansvar at være opdateret på gældende lovgivning til enhver tid. OCMA masterclass kan ikke påtage sig ansvar, hverken helt eller delvist, for eventuelle lovovertrædelser, der måtte opstå som følge af elevens handlinger.
-            </p>
+            Det er elevens eget ansvar at være opdateret på gældende lovgivning til enhver tid. OCMA masterclass kan ikke påtage sig ansvar, hverken helt eller delvist, for eventuelle lovovertrædelser, der måtte opstå som følge af elevens handlinger.            </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
               Fortrolighed og GDPR
             </h1>
             <p className="text-[18px]">
-            Oplysninger må på ingen måde deles med tredjepart og må ikke udnyttes, herunder også efter aftalens udløb. Begge parter forpligter sig til at behandle oplysningerne på en måde, der sikrer, at de forbliver utilgængelige for uvedkommende, og at der ikke opstår nogen risiko for, at uvedkommende får kendskab til dem. Det er kundens ansvar at fjerne OCMA Masterclass´s adgang til alt, der vedrører persondata.            </p>
+            Oplysninger må på ingen måde deles med tredjepart og må ikke udnyttes, herunder også efter aftalens udløb. Begge parter forpligter sig til at behandle oplysningerne på en måde, der sikrer, at de forbliver utilgængelige for uvedkommende, og at der ikke opstår nogen risiko for, at uvedkommende får kendskab til dem. Det er kundens ansvar at fjerne OCMA Masterclass´s adgang til alt, der vedrører persondata.             </p>
           </div>
           <div className="mb-[50px]">
             <h1 className="text-[30px] font-[700] mb-[20px]">
@@ -133,6 +130,7 @@ Kunden/eleven har ansvaret for at acceptere at denne masterclass er et produkt t
             </h1>
             <p className="text-[18px]">
             Eventuelle uoverensstemmelser, der opstår som følge af disse betingelser eller andre samhandelsforhold mellem parterne, skal indbringes for retten i København. Tvisten vil blive afgjort i overensstemmelse med dansk ret.            </p>
+            <p className="mt-20">OPDATERET: d. 1 April</p>
           </div>
         </div>
       </section>
