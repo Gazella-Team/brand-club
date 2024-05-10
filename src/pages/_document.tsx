87549beb917418efe6873d76645185f2b0385615
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-10 21:47:36 +0200

    push

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Meta from "@/components/Layout/Meta";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script src="https://api.toolbird.io/js/script.js" strategy="lazyOnload" data-domain="ocmamasterclass.com"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
