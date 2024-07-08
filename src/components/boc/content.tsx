8ca65d34afc532f5344b5233bacccb747edfa2a5
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-08 15:47:54 +0200

    e

--- src/components/boc/content.tsx
+++ src/components/boc/content.tsx
@@ -23,43 +23,38 @@ export default function BocContent() {
           </p>
         </Reveal>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-          <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="text-white bg-[#29a226] p-6 flex flex-col gap-6 rounded-2xl text-left">
+          <div className="text-white bg-green-900 p-6 flex flex-col gap-6 rounded-2xl text-left">
             <img className="rounded-2xl" src="/boc/contents/1.webp"></img>
             <div className="flex flex-col gap-1">
               <h1 className="text-xl font-semibold">Markedsføring hjælp fra eksperter</h1>
               <p>Direkte hjælp & viden fra dem der hjalp os med vores markedsføring. </p>
             </div>
           </div>
-          <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="text-white p-6 flex flex-col gap-6 rounded-2xl text-left">
-            <img className="rounded-2xl" src="/boc/contents/2.webp"></img>
+          <div className="text-white bg-green-900 p-6 flex flex-col gap-6 rounded-2xl text-left">            <img className="rounded-2xl" src="/boc/contents/2.webp"></img>
             <div className="flex flex-col gap-1">
               <h1 className="text-xl font-semibold">Hjælp til skalering af dit brand</h1>
               <p>Trin for trin gennemgang fra professionelle webudviklere </p>
             </div>
           </div>
-          <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="text-white p-6 flex flex-col gap-6 rounded-2xl text-left">
-            <img className="rounded-2xl" src="/boc/contents/3.webp"></img>
+          <div className="text-white bg-green-900 p-6 flex flex-col gap-6 rounded-2xl text-left">            <img className="rounded-2xl" src="/boc/contents/3.webp"></img>
             <div className="flex flex-col gap-1">
               <h1 className="text-xl font-semibold">Liste med eksklusive Leverandører</h1>
               <p>Modtag liste med eksklusive leverandører vi selv tager i brug hver dag</p>
             </div>
           </div>
-          <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="text-white p-6 flex flex-col gap-6 rounded-2xl text-left">
-            <img className="rounded-2xl" src="/boc/contents/4.webp"></img>
+          <div className="text-white bg-green-900 p-6 flex flex-col gap-6 rounded-2xl text-left">            <img className="rounded-2xl" src="/boc/contents/4.webp"></img>
             <div className="flex flex-col gap-1">
               <h1 className="text-xl font-semibold">Opbyggelse af brand</h1>
               <p>Lær hvordan du finder din stil til dit brand og hvordan du brander det til millioner.</p>
             </div>
           </div>
-          <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="text-white p-6 flex flex-col gap-6 rounded-2xl text-left">
-            <img className="rounded-2xl" src="/boc/contents/5.webp"></img>
+          <div className="text-white bg-green-900 p-6 flex flex-col gap-6 rounded-2xl text-left">            <img className="rounded-2xl" src="/boc/contents/5.webp"></img>
             <div className="flex flex-col gap-1">
               <h1 className="text-xl font-semibold">Danmarks førende fællesskab for brand ejere</h1>
               <p>Dette er Danmarks førende fællesskab for brandejere, hvor ambitiøse brandejere netværker og lader sig inspirere af hinanden.</p>
             </div>
           </div>
-          <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="text-white p-6 flex flex-col gap-6 rounded-2xl text-left">
-            <img className="rounded-2xl" src="/boc/contents/6.webp"></img>
+          <div className="text-white bg-green-900 p-6 flex flex-col gap-6 rounded-2xl text-left">            <img className="rounded-2xl" src="/boc/contents/6.webp"></img>
             <div className="flex flex-col gap-1">
               <h1 className="text-xl font-semibold">Adgang til eksklusive events</h1>
               <p>Gennem brand owners club får du adgang til eksklusive FCFS Events</p>
--- src/components/boc/globus.tsx
+++ src/components/boc/globus.tsx
@@ -16,15 +16,15 @@ export default function Globus() {
         </section>
         <section>
             <div className="w-sectionMain mt-[-400px] mx-auto max-w-6xl text-center grid grid-cols-1 lg:grid-cols-3 gap-6">
-                <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="p-10 rounded-2xl flex flex-col gap-4 border-2 border-white">
+                <div className="p-10 rounded-2xl bg-green-900 flex flex-col gap-4 border-2 border-white">
                     <h2 className="font-semibold text-left text-xl">Fra 0  -  2.000.000 på 6 måneder</h2>
                     <p className="text-left">Gustav Aabro og Filip Mellbin er ejerne bag tøjbrandet Ngoodso. De gik fra 0 - 2.000.000,- i omsætning på under 6 måneder. Uden hverken tårnhøje annoncebudgetter, men faktisk primært grundet deres unikke forståelse for branding, organisk markedsføring og kreative designs. Nu lærer de dig om hvordan du kan gøre netop det samme.</p>
                 </div>
-                <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="p-10 rounded-2xl flex flex-col gap-4 border-2 border-white">
+                <div className="p-10 rounded-2xl bg-green-900  flex flex-col gap-4 border-2 border-white">
                     <h2 className="font-semibold text-left text-xl">Millionforretning som teenagers</h2>
                     <p className="text-left">Trods Gustav og filips unge alder på 17 & 21 kunne de formå at få stor succes med deres tøjbrand.</p>
                 </div>
-                <div style={{background: "linear-gradient(90deg, rgba(40,80,23,1) 0%, rgba(33,150,10,1) 100%)"}} className="p-10 rounded-2xl flex flex-col gap-4 border-2 border-white">
+                <div className="p-10 rounded-2xl bg-green-900  flex flex-col gap-4 border-2 border-white">
                     <h2 className="font-semibold text-left text-xl">Fik hjælp udefra</h2>
                     <p className="text-left">Gustav & Filip succes skyldtes ikke blot deres talent, men snarere deres evne til at være villig til at række ud til kloge hoveder indenfor specifikke felter. I brand owners club modtager du bla. viden fra store markedsførings specialister.</p>
                 </div>
